1a7f662fd8eb61ad2be81d0703a58ff992879056
DonkeyKong245 <super.user.maa@gmail.com>
2020-04-26 13:26:11 +0300

    Set up project configuration for proper Dockerization and Heroku deployment

--- RecyclingBot/RecyclingBot/Program.cs
+++ RecyclingBot/RecyclingBot/Program.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using System;
 using System.IO;
 
 namespace RecyclingBot
 {
   public class Program
   {
+    public static string Port
+    {
+      get
+      {
+        const string portVariableName = "PORT";
+        string port = Environment.GetEnvironmentVariable(portVariableName);
+
+        if (string.IsNullOrWhiteSpace(port))
+        {
+          Console.WriteLine("{0} is not set", portVariableName);
+        }
+
+        return port;
+      }
+    }
+
     public static void Main(string[] args)
     {
       new WebHostBuilder()
@@ -12,9 +29,11 @@ namespace RecyclingBot
         .UseContentRoot(Directory.GetCurrentDirectory())
         .UseIISIntegration()
         .UseStartup<Startup>()
+        .UseUrls($"http://*:{Port}")  
+        //  We have to listen on port provided by Heroku
         .UseApplicationInsights()
         .Build()
         .Run();
-    }
+    }    
   }
 }
